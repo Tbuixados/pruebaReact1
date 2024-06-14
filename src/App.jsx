5c252fc3d26dc2866fc0f58763c4e071698a40af
Tbuixados <buixadostomas@gmail.com>
2024-06-14 19:50:33 -0300

    primeros archivos

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
+function App() {
+  return (
+    <div>
+      <h1 className="titulo">Este es el titulo</h1>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
